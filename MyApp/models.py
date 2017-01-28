3e8ed995d4518a2917af49f8bef985cb955c0c11
Mohamed Ayman <m950603@gmail.com>
2017-01-29 00:33:00 +0200

    Create Data Base and Model

--- MyApp/models.py
+++ MyApp/models.py
@@ -1,5 +1,10 @@
 from __future__ import unicode_literals
-
+from datetime import datetime
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Weather (models.Model) :
+    temprature = models.FloatField(default=None,validators=[MaxValueValidator(28), MinValueValidator(19)])
+    humidity   = models.FloatField(default=None,validators=[MaxValueValidator(65), MinValueValidator(35)])
+    date = models.DateTimeField(default=datetime.now)
